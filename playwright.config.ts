46f2b9b2d85874ffcb505b1a1cd3038bdaec08f0
Mateo Agudelo Garcia <mateo.agudelo80@eia.edu.co>
2025-04-03 17:07:08 -0500

    chore: improve ci implementation

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,9 +18,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries: 3,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -29,7 +31,7 @@ export default defineConfig({
     baseURL: 'https://parabank.parasoft.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-all-retries',
   },
 
   /* Configure projects for major browsers */
